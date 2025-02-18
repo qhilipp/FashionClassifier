b60758114f318307e26e583846aab1526f792527
Philipp Kathöfer <philippkathoefr@gmail.com>
2025-02-17 17:35:33 -0800

    Use Optuna for hyper parameter optimization

--- model.py
+++ model.py
@@ -6,15 +6,19 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 import torch.nn as nn
 import argparse
+import optuna
 
-parser = argparse.ArgumentParser(description="A DeepLearning model using PyTorch to classify Fashion MNIST images")
+parser = argparse.ArgumentParser(description="A DeepLearning model using PyTorch and Optuna to classify Fashion MNIST images")
 
+parser.add_argument('-l', '--load', type=str, help='The name of the file to load the model from', default=None)
+parser.add_argument('-s', '--save', type=str, help='The name of the file the model should be saved to', default=None)
 parser.add_argument('-d', '--device', type=str, help='The device on which PyTorch should do all the Tensor calculations on, defaults to \'cpu\' if not available', default='cpu')
 parser.add_argument('-e', '--epochs', type=int, help='The number of epochs used to train the model', default=20)
-parser.add_argument('-l', '--learnrate', type=float, help='The learnrate for the optimizer', default=0.001)
-parser.add_argument('-b', '--batchsize', type=int, help='The batchsize for the data loader', default=64)
+parser.add_argument('-t', '--trials', type=int, help='The number of trials that are made to find optimal hyper parameters', default=5)
+parser.add_argument('-te', '--trial_epochs', type=int, help='The number of epochs used per trial to train the model when finding optimal hyper parameters', default=2)
 
 args = parser.parse_args()
+plt.ion()
 
 device = torch.device(args.device if torch.backends.mps.is_available() else 'cpu')
 
@@ -43,15 +47,18 @@ class Model(nn.Module):
         x = x.view(x.size(0), 1, 28, 28)
         return self.stack(x)
 
-def train(model, data_loader, optimizer, loss_function, epochs):
-    print("Training model...")
+def train(model, data_loader, optimizer, loss_function, epochs, verbose=False) -> float:
+    if verbose:
+        print("Training model...")
 
     model.train()
     loss_averages = []
 
     for epoch in range(epochs):
         losses = []
-        print(f"Epoch {epoch+1}/{epochs}")
+        if verbose:
+            print(f"Epoch {epoch+1}/{epochs}")
+
         for i, (x, y) in enumerate(data_loader):
             x = x.to(device)
             y = y.to(device)
@@ -60,20 +67,28 @@ def train(model, data_loader, optimizer, loss_function, epochs):
             loss = loss_function(prediction, y)
             loss.backward()
             optimizer.step()
+
             losses.append(loss.item())
-            print(f'\r{(i+1) / len(data_loader) * 100:.2f}%', end='', flush=True)
+            if verbose:
+                print(f'\r{(i+1) / len(data_loader) * 100:.2f}%', end='', flush=True)
+
         loss_averages.append(np.array(losses).mean())
-        print()
+        if verbose:
+            print()
 
-    plt.plot(loss_averages)
-    plt.title("Training loss")
-    plt.xlabel("Epoch")
-    plt.ylabel("Loss")
+    if verbose:
+        plt.plot(loss_averages)
+        plt.title("Training loss")
+        plt.xlabel("Epoch")
+        plt.ylabel("Loss")
 
-    print(f"\nDone, the final loss is {loss_averages[-1]}")
+    if verbose:
+        print(f"\nDone, the final loss is a loss of {loss_averages[-1]}")
+
+    return loss_averages[-1]
 
 
-def evaluate(model, test_data):
+def evaluate(model, test_data) -> float:
     print("Evaluating model...")
 
     model.eval()
@@ -89,6 +104,26 @@ def evaluate(model, test_data):
     print(f"\nDone, the model has an accuracy of {evaluation * 100}%")
 
     return evaluation
+
+def objective(trial):
+    learn_rate = trial.suggest_float("learn_rate", 1e-5, 1e-1, log=True)
+    batch_size = trial.suggest_int("batch_size", 32, 128, log=True)
+
+    model = Model().to(device)
+
+    print(f"Trial {trial.number+1}/{args.trials}")
+
+    loss = train(
+        model,
+        DataLoader(training_data, batch_size, True),
+        torch.optim.Adam(model.parameters(), learn_rate),
+        nn.CrossEntropyLoss(),
+        epochs=args.trial_epochs
+    )
+
+    print(f"Done, arguments {trial.params} resulted in {loss}")
+
+    return loss
 
 print("Fetching data...")
 
@@ -106,16 +141,30 @@ test_data = datasets.FashionMNIST(
     transform=ToTensor()
 )
 
-model = Model().to(device)
+if args.load is not None:
+    model = torch.load(f"models/{args.load}", weights_only=False)
+else:
+    print("Finding approximation for optimal hyper parameters...")
 
-train(
-    model,
-    DataLoader(training_data, args.batchsize, True),
-    torch.optim.Adam(model.parameters(), args.learnrate),
-    nn.CrossEntropyLoss(),
-    epochs=args.epochs
-)
+    study = optuna.create_study(direction='minimize') #, pruner=optuna.pruners.MedianPruner())
+    study.optimize(objective, n_trials=args.trials)
+
+    print(f"Done, found {study.best_params} as optimal hyper parameters")
+
+    model = Model().to(device)
+
+    train(
+        model,
+        DataLoader(training_data, study.best_params['batch_size'], True),
+        torch.optim.Adam(model.parameters(), study.best_params['learn_rate']),
+        nn.CrossEntropyLoss(),
+        epochs=args.epochs,
+        verbose=True
+    )
+
+    plt.show()
 
 evaluate(model, test_data)
 
-plt.show()
+if args.save is not None:
+    torch.save(model, f"models/{args.save}")
